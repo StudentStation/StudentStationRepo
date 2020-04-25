bf7ce0a5b61b9f4b13d773bb86d0a7e4ff8511a6
ConnorLynch <ConnorLynch@localhost>
2020-04-24 21:27:59 -0500

    all HTML pages complete

--- webproject/src/SimpleFormInsert.java
+++ webproject/src/SimpleFormInsert.java
@@ -55,14 +55,72 @@ public class SimpleFormInsert extends HttpServlet {
       // Set response content type
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
-      String title = "Insert Data to DB table";
       String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h2 align=\"center\">" + title + "</h2>\n" + //
-            "<ul>\n" + //
+      out.println(docType + "<html>\r\n" + 
+        		"<head><style>\r\n" + 
+          		"header {\r\n" + 
+          		"    background-color:black;\r\n" + 
+          		"    color: white;\r\n" + 
+          		"    width: 100%;\r\n" + 
+          		"    height: 100px;\r\n" + 
+          		"    padding: 5px;\r\n" + 
+          		"}\r\n" + 
+          		"nav {\r\n" + 
+          		"    line-height:30px;\r\n" + 
+          		"    background-color:#eeeeee;\r\n" + 
+          		"    opacity: 60%;\r\n" + 
+          		"    height:300px;\r\n" + 
+          		"    width: 10%;\r\n" + 
+          		"    float:left;\r\n" + 
+          		"    padding:5px;	      \r\n" + 
+          		"}\r\n" + 
+          		"section {\r\n" + 
+          		"    width: 85%;\r\n" + 
+          		"    float:left;\r\n" + 
+          		"    padding:10px;\r\n" + 
+          		"    display: inline;\r\n" + 
+          		"	background-color: white;\r\n" + 
+          		"	opacity: 80%;\r\n" + 
+          		"	margin: 1%;	 	 \r\n" + 
+          		"}\r\n" + 
+          		"body {\r\n" + 
+          		"	background-image: url(\"/webproject/train background.jpg\");\r\n" + 
+          		"    background-repeat: no-repeat;\r\n" + 
+          		"    background-size: 100% 100%;\r\n" + 
+          		"    overflow: hidden;\r\n" + 
+          		"   	width: 100%;\r\n" + 
+          		"   	margin: 0 auto 0 auto;\r\n" + 
+          		"}\r\n" + 
+          		".logo {\r\n" + 
+          		"	width: 10%;\r\n" + 
+          		"	height: 100%;\r\n" + 
+          		"	text-align: left;\r\n" + 
+          		"	display: inline;\r\n" + 
+          		"}\r\n" + 
+          		".title {\r\n" + 
+          		"	text-align: center;\r\n" + 
+          		"	display: inline;\r\n" + 
+          		"	position: absolute;\r\n" + 
+          		"	left: 40%;\r\n" + 
+          		"	top: -20px;\r\n" + 
+          		"	font-size: 60px;\r\n" + 
+          		"}\r\n" + 
+          		"</style>\r\n" + 
+          		"</head>\r\n" + 
+          		"\r\n" + 
+          		"<body>\r\n" + 
+          		"<header>\r\n" + 
+          		"	<h1 class = \"title\">Create A Page</h1>\r\n" + 
+          		"	<img class= \"logo\" src=\"/webproject/student station logo.png\" alt=\"Student Station\">\r\n" + 
+          		"</header>\r\n" + 
+          		"\r\n" + 
+          		"<nav>\r\n" + 
+          		"<a href=\"/webproject/home.html\">Home</a> <br>\r\n" + 
+          		"<a href=\"/webproject/simpleFormSearch.html\">Search Students</a> <br>\r\n" + 
+          		"<a href=\"/webproject/simpleFormInsert.html\">Create A Page</a> <br>\r\n" + 
+          		"</nav>\r\n" + 
+          		"\r\n" + 
+          		"<section>\r\n" +
 
             "  <li><b>Name</b>: " + name + "\n" + //
             "  <li><b>Email</b>: " + email + "\n" + //
@@ -70,12 +128,11 @@ public class SimpleFormInsert extends HttpServlet {
             "  <li><b>Minor</b>: " + minor + "\n" + //
             "  <li><b>Organizations</b>: " + org + "\n" + //
             "  <li><b>Graduation</b>: " + grad + "\n" + //
-            "  <li><b>Bio</b>: " + bio + "\n" + //
+            "  <li><b>Bio</b>: " + bio + "\n" +"<br>"
+            );
 
-            "</ul>\n");
-
-      out.println("<a href=/webproject/simpleFormSearch.html>Search Data</a> <br>");
-      out.println("</body></html>");
+      out.println("<a href=/webproject/home.html>Go Home</a> <br>");
+      out.println("</section></body></html>");
    }
 
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- webproject/src/SimpleFormSearch.java
+++ webproject/src/SimpleFormSearch.java
@@ -27,14 +27,77 @@ public class SimpleFormSearch extends HttpServlet {
    void search(String keyword, HttpServletResponse response) throws IOException {
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
-      String title = "Database Result";
       String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
             "transitional//en\">\n"; //
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h1 align=\"center\">" + title + "</h1>\n");
+      out.println(docType + "<html>\r\n" + 
+      		"<head><style>\r\n" + 
+      		"header {\r\n" + 
+      		"    background-color:black;\r\n" + 
+      		"    color: white;\r\n" + 
+      		"    width: 100%;\r\n" + 
+      		"    height: 100px;\r\n" + 
+      		"    padding: 5px;\r\n" + 
+      		"}\r\n" + 
+      		"nav {\r\n" + 
+      		"    line-height:30px;\r\n" + 
+      		"    background-color:#eeeeee;\r\n" + 
+      		"    opacity: 60%;\r\n" + 
+      		"    height:300px;\r\n" + 
+      		"    width: 10%;\r\n" + 
+      		"    float:left;\r\n" + 
+      		"    padding:5px;	      \r\n" + 
+      		"}\r\n" + 
+      		"section {\r\n" + 
+      		"    width: 85%;\r\n" + 
+      		"    float:left;\r\n" + 
+      		"    padding:10px;\r\n" + 
+      		"    display: inline;\r\n" + 
+      		"	background-color: white;\r\n" + 
+      		"	opacity: 80%;\r\n" + 
+      		"	margin: 1%;	 	 \r\n" + 
+      		"}\r\n" + 
+      		"body {\r\n" + 
+      		"	background-image: url(\"/webproject/train background.jpg\");\r\n" + 
+      		"    background-repeat: no-repeat;\r\n" + 
+      		"    background-size: 100% 100%;\r\n" + 
+      		"    overflow: hidden;\r\n" + 
+      		"   	width: 100%;\r\n" + 
+      		"   	margin: 0 auto 0 auto;\r\n" + 
+      		"}\r\n" + 
+      		".logo {\r\n" + 
+      		"	width: 10%;\r\n" + 
+      		"	height: 100%;\r\n" + 
+      		"	text-align: left;\r\n" + 
+      		"	display: inline;\r\n" + 
+      		"}\r\n" + 
+      		".title {\r\n" + 
+      		"	text-align: center;\r\n" + 
+      		"	display: inline;\r\n" + 
+      		"	position: absolute;\r\n" + 
+      		"	left: 40%;\r\n" + 
+      		"	top: -20px;\r\n" + 
+      		"	font-size: 60px;\r\n" + 
+      		"}\r\n" + 
+      		"</style>\r\n" + 
+      		"</head>\r\n" + 
+      		"\r\n" + 
+      		"<body>\r\n" + 
+      		"<header>\r\n" + 
+      		"	<h1 class = \"title\">Search Result</h1>\r\n" + 
+      		"	<img class= \"logo\" src=\"/webproject/student station logo.png\" alt=\"Student Station\">\r\n" + 
+      		"</header>\r\n" + 
+      		"\r\n" + 
+      		"<nav>\r\n" + 
+      		"<a href=\"/webproject/home.html\">Home</a> <br>\r\n" + 
+      		"<a href=\"/webproject/simpleFormSearch.html\">Search Students</a> <br>\r\n" + 
+      		"<a href=\"/webproject/simpleFormInsert.html\">Create A Page</a> <br>\r\n" + 
+      		"</nav>\r\n" + 
+      		"\r\n" + 
+      		"<section>\r\n" + "<table style='width:100%'>\r\n" +
+            "<tr><th align='left'>Name</th><th align='left'>Email</th><th align='left'>Major</th><th align='left'>" + 
+            "Minor</th><th align='left'>Organizations</th><th align='left'>Graduation date</th></tr>" +
+              "</ul>\n"
+      		);
 
       Connection connection = null;
       PreparedStatement preparedStatement = null;
@@ -72,16 +135,17 @@ public class SimpleFormSearch extends HttpServlet {
 
 
             if (keyword == null || keyword.isEmpty() || name.contains(keyword) || email.contains(keyword) || major.contains(keyword) || minor.contains(keyword) || org.contains(keyword) || grad.contains(keyword)) {
-               out.println("Name: " + name + ", ");
-               out.println("Email: " + email + ", ");
-               out.println("Major: " + major + ", ");
-               out.println("Minor: " + minor + ", ");
-               out.println("Organizations: " + org + ", ");
-               out.println("Graduation Date: " + grad + "<br>");
+               out.println("<tr>\t<td>" + name + "</td>");
+               out.println("<td>" + email + "</td>");
+               out.println("<td>" + major + "</td>");
+               out.println("<td>" + minor + "</td>");
+               out.println("<td>" + org + "</td>");
+               out.println("<td>" + grad + "</td></tr><br>");
             }
          }
-         out.println("<a href=/webproject/simpleFormSearch.html>Search Data</a> <br>");
-         out.println("</body></html>");
+         out.println("</table>");
+         out.println("<a href=/webproject/simpleFormSearch.html>Back to Search</a> <br>");
+         out.println("</section></body></html>");
          rs.close();
          preparedStatement.close();
          connection.close();
